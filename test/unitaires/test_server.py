93cf0c54dcd41595f003052fbd3d84957109b60b
ydjabela <ydjabela@blendbow.com>
2022-10-10 15:16:43 +0200

    Revert "Qa"

--- test/unitaires/test_server.py
+++ test/unitaires/test_server.py
@@ -1,7 +1,6 @@
 import pytest
-from Projet_11_OpenClassrooms.server import create_app
-from Projet_11_OpenClassrooms.models.club import Club_model
-from Projet_11_OpenClassrooms.models.competitions import Competitions_model
+from ... import server
+from ... server import create_app
 clubs = [
     {
         "name": "Cinho-Club",
@@ -37,26 +36,8 @@ competitions = [
     }
 ]
 
-
 @pytest.fixture
-def client(mocker):
-    clubs = [Club_model("Cinho-Club", "cinhoclub@gmail.com", "15"),
-             Club_model("Raf-Club", "rafclub@gmail.com", "10"),
-             Club_model("Francoise-Club", "Francoiseclub@gmail.com", "5")
-             ]
-    competitions =[
-        Competitions_model("first competitions", "2022-09-26 00:19:00", "15"),
-        Competitions_model("second competitions", "2022-09-28 00:19:00", "20"),
-        Competitions_model("third competitions", "2022-09-30 00:19:00", "5")
-    ]
-    mocker.patch(
-        'Projet_11_OpenClassrooms.repository.loadclub.Club.load_clubs',
-        return_value=clubs
-    )
-    mocker.patch(
-        'Projet_11_OpenClassrooms.repository.loadcompetitions.Competitions.load_competition',
-        return_value=competitions
-    )
+def client():
     app = create_app()
     with app.test_client() as client:
         yield client
@@ -69,120 +50,67 @@ def test_should_status_code_ok(client):
     assert "Welcome to the GUDLFT Registration Portal" in data
 
 
-def test_show_summary(client, mocker):
-    club_1 = Club_model("Cinho-Club", "cinhoclub@gmail.com", "15")
-    mocker.patch('Projet_11_OpenClassrooms.repository.loadclub.Club.load_clubs_by_email', return_value=club_1)
+def test_show_summary(client):
     # if email is ok
-    response_1 = client.post("/showSummary", data={"email": club_1.email})
-    data_1 = response_1.data.decode()
-    assert response_1.status_code == 200
-    assert data_1.find("<title>GUDLFT Registration</title>") == -1
-    assert "Welcome, cinhoclub@gmail.com" in data_1
+    response = client.post("/showSummary", data={"email": clubs[1]["email"]})
+    data = response.data.decode()
+    assert response.status_code == 200
+    assert "Welcome, {}".format(clubs[1]["email"]) in data
 
     # if email is not ok
-    club_2 = None
-    mocker.patch('Projet_11_OpenClassrooms.repository.loadclub.Club.load_clubs_by_email', return_value=club_2)
-    response_2 = client.post("/showSummary", data={"email": "email_not_ok@test@com"})
-    data_2 = response_2.data.decode()
-    assert response_2.status_code == 404
-    assert data_2.find("<title>GUDLFT Registration</title>") != -1
+    response = client.post("/showSummary", data={"email": clubs[0]["email"]})
+    assert response.status_code == 404
 
     # if email is not exist
-    club_3 = None
-    mocker.patch('Projet_11_OpenClassrooms.repository.loadclub.Club.load_clubs_by_email', return_value=club_3)
-    response_3 = client.post("/showSummary", data={"email": ""})
-    assert response_3.status_code == 404
+    response = client.post("/showSummary", data={"email": clubs[2]["email"]})
+    assert response.status_code == 404
 
 
-def test_book(client, mocker):
+def test_book(client):
     # if competition and club  founded
-    club_1 = Club_model("Cinho-Club", "cinhoclub@gmail.com", "15")
-    mocker.patch('Projet_11_OpenClassrooms.repository.loadclub.Club.load_clubs_by_name', return_value=club_1)
-    competition_1 = Competitions_model("first competitions", "2022-09-26 00:19:00", "15")
-    mocker.patch('Projet_11_OpenClassrooms.repository.loadcompetitions.Competitions.load_competition_by_name', return_value=competition_1)
-    competition_nbr_of_places = competition_1.numberOfPlaces
-    response = client.get("/book/{}/{}".format(competition_1.name, club_1.name))
+    competition_name = competitions[0]['name']
+    competition_nbr_of_places = competitions[0]['numberOfPlaces']
+    clubs_name = clubs[1]['name']
+    response = client.get("/book/{}/{}".format(competition_name, clubs_name))
     data = response.data.decode()
     assert response.status_code == 200
     assert "Places available: {}".format(competition_nbr_of_places) in data
 
     # if competition and club not founded
-    club_2 = None
-    mocker.patch('Projet_11_OpenClassrooms.repository.loadclub.Club.load_clubs_by_name', return_value=club_2)
-    competition_2 = None
-    mocker.patch('Projet_11_OpenClassrooms.repository.loadcompetitions.Competitions.load_competition_by_name', return_value=competition_2)
-    response = client.get("/book/competitionsnotok/Club not exist")
-    assert response.status_code == 404
+    competition_name = competitions[1]['name']
+    clubs_name = clubs[0]['name']
+    response = client.get("/book/{}/{}".format(competition_name, clubs_name))
+    assert response.status_code == 500
 
     # if club not founded
-    mocker.patch('Projet_11_OpenClassrooms.repository.loadclub.Club.load_clubs_by_name', return_value=club_2)
-    mocker.patch('Projet_11_OpenClassrooms.repository.loadcompetitions.Competitions.load_competition_by_name', return_value=competition_1)
-    response = client.get("/book/{}/Club not exist".format(competition_1.name))
-    assert response.status_code == 404
+    competition_name = competitions[0]['name']
+    clubs_name = clubs[0]['name']
+    response = client.get("/book/{}/{}".format(competition_name, clubs_name))
+    assert response.status_code == 500
 
     # if competition  not founded
-    mocker.patch('Projet_11_OpenClassrooms.repository.loadclub.Club.load_clubs_by_name', return_value=club_1)
-    mocker.patch('Projet_11_OpenClassrooms.repository.loadcompetitions.Competitions.load_competition_by_name', return_value=competition_2)
-    response = client.get("/book/{}/Cinho-Club".format(club_1.name))
-    assert response.status_code == 404
+    competition_name = competitions[1]['name']
+    clubs_name = clubs[1]['name']
+    response = client.get("/book/{}/{}".format(competition_name, clubs_name))
+    assert response.status_code == 500
 
 
-def test_purchase_places_ok(client, mocker):
-    club_1 = Club_model("Cinho-Club", "cinhoclub@gmail.com", "15")
-    mocker.patch('Projet_11_OpenClassrooms.repository.loadclub.Club.load_clubs_by_name', return_value=club_1)
-    competition_1 = Competitions_model("first competitions", "2022-09-26 00:19:00", "25")
-    mocker.patch('Projet_11_OpenClassrooms.repository.loadcompetitions.Competitions.load_competition_by_name',
-                 return_value=competition_1)
+def test_purchase_places(client):
     response = client.post(
-        '/purchasePlaces',
+        "/purchasePlaces",
         data={
-            'competition': competition_1.name,
-            'club': club_1.name,
-            'places': 4
+            "competition": competitions[0]['name'],
+            "club": clubs[1]['name'],
+            "places": 13
         }
     )
-    data_1 = response.data.decode()
+    current_points = int(clubs[1]['points'])
+    current_places = int(competitions[0]['numberOfPlaces'])
+    data = response.data.decode()
     assert response.status_code == 200
-    assert "Welcome, {}".format(club_1.email) in data_1
-    assert data_1.find("<title>Summary | GUDLFT Registration</title>") != -1
-    assert data_1.find('Great-booking complete!') != -1
-    assert club_1.points == '15'  # TODO correct  bug number  of club places
-    assert competition_1.numberOfPlaces == 21  # TODO correct  bug int -> str
-
-
-def test_purchase_places_nok(client, mocker):
-    for places in [-2, 0, 4, 6, 13]:
-        club_1 = Club_model("Cinho-Club", "cinhoclub@gmail.com", "15")
-        mocker.patch('Projet_11_OpenClassrooms.repository.loadclub.Club.load_clubs_by_name', return_value=club_1)
-        competition_1 = Competitions_model("first competitions", "2022-09-26 00:19:00", "25")
-        mocker.patch('Projet_11_OpenClassrooms.repository.loadcompetitions.Competitions.load_competition_by_name',
-                     return_value=competition_1)
-        response = client.post(
-            '/purchasePlaces',
-            data={
-                'competition': competition_1.name,
-                'club': club_1.name,
-                'places': places
-            }
-        )
-        data_1 = response.data.decode()
-        assert response.status_code == 200
-        assert "Welcome, {}".format(club_1.email) in data_1
-        assert data_1.find("<title>Summary | GUDLFT Registration</title>") != -1
-        if places <= 0:
-            # assert data_1.find('the  number  of  places need to be not negative') != -1  # TODO
-            pass
-        elif places > 12:
-            pass
-            # assert data_1.find('the  number  of  places need to be under to 12') != -1   # TODO
-        else:
-            # if number of  places > int(club_1.points)/3 = 5
-            if places >= 5:
-                assert club_1.points == '15'  # TODO correct  bug number  of club places ==> 15 - places *3
-                assert competition_1.numberOfPlaces == 25 - places  # TODO correct  bug int -> str
-            else:
-                pass
-                # assert data_1.find('Not enough points ') != -1
+    assert "Welcome, {}".format(clubs[1]["email"]) in data
+    assert str.encode(f"Points available: {current_points}") in response.data
+    assert str.encode(f"Number of Places: {current_places}") in response.data
 
 
 def test_logout(client):
@@ -190,3 +118,6 @@ def test_logout(client):
     data = response.data.decode()
     assert response.status_code == 200
     assert "Welcome to the GUDLFT Registration Portal" in data
+
+
+
