53208e995d6430ce5797ab482dc0099a928a83bd
ydjabela <ydjabela@blendbow.com>
2022-10-25 13:44:59 +0200

    update excpet

--- repository/loadcompetitions.py
+++ repository/loadcompetitions.py
@@ -24,7 +24,7 @@ class Competitions(Competitions_model):
         try:
             foundCompetition = [c for c in competitions if c['name'] == competition_name][0]
             return foundCompetition
-        except TypeError or IndexError:
+        except (TypeError, IndexError) as e:
             return None
 
     # -----------------------------------------------------------------------------------------------------------------#
